8785cb9c9e31ebc52347c9809f51260c908d8a5b
unknown <danielweston37@gmail.com>
2024-12-22 08:14:43 +0000

    Added customisable build script

---
+++ build_extension.sh
@@ -0,0 +1,5 @@
+blender_exe="C:/Apps/Blender/stable/blender-4.3.0-stable.2b18cad88b13/blender.exe"
+
+cmd_args="--command extension build --source-dir D:/git/blender_sfdi/blender_sfdi"
+
+eval "${blender_exe} ${cmd_args}"
